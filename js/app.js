ff5ac0626f9d5e0dbe17e8872c0547e384a2b936
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-04-04 22:57:06 -0300

    se agrega la logica del programa y se lo deja funcional

--- js/app.js
+++ js/app.js
@@ -1,26 +1,38 @@
 let opciones = ["piedra", "papel", "tijera"];
+let eleccionUsuario;
 let resultado = "";
+let bandera = false;
+const btnPiedra = document.getElementById('btnPiedra');
+const btnPapel = document.getElementById('btnPapel');
+const btnTijera = document.getElementById('btnTijera');
+let contenedorGanador = document.getElementById('contenedorGanador');
 
-do{
-  // let eleccionUsuario = prompt("Elige piedra, papel o tijera").toLowerCase();
-  while (!opciones.includes(eleccionUsuario)) {
-    eleccionUsuario = prompt("Eleccion invalida. Elige piedra, papel o tijera").toLowerCase();
+btnPiedra.addEventListener('click', piedra);
+btnPapel.addEventListener('click', papel);
+btnTijera.addEventListener('click', tijera);
+
+function piedra() { eleccionUsuario = 0; bandera = true; juego();}
+function papel() { eleccionUsuario = 1; bandera = true; juego();}
+function tijera() { eleccionUsuario = 2; bandera = true; juego();}
+
+
+function juego (){
+  if (bandera === true) {
+    let eleccionPC = opciones[Math.floor(Math.random() * opciones.length)];
+  
+    if (opciones[eleccionUsuario] === eleccionPC) {
+      resultado = "Empate";
+    } else if (
+      (opciones[eleccionUsuario] === "piedra" && eleccionPC === "tijera") ||
+      (opciones[eleccionUsuario] === "papel" && eleccionPC === "piedra") ||
+      (opciones[eleccionUsuario] === "tijera" && eleccionPC === "papel")
+    ) {
+      resultado = "Ganaste";
+    } else {
+      resultado = "Perdiste";
+    }
+  
+      contenedorGanador.innerHTML = `Elegiste ${opciones[eleccionUsuario]}. La PC eligio ${eleccionPC}. ${resultado}!`;
   }
+}
 
-  let eleccionPC = opciones[Math.floor(Math.random() * opciones.length)];
-
-  if (eleccionUsuario === eleccionPC) {
-    resultado = "Empate";
-  } else if (
-    (eleccionUsuario === "piedra" && eleccionPC === "tijera") ||
-    (eleccionUsuario === "papel" && eleccionPC === "piedra") ||
-    (eleccionUsuario === "tijera" && eleccionPC === "papel")
-  ) {
-    resultado = "Ganaste";
-  } else {
-    resultado = "Perdiste";
-  }
-
-  alert(`Elegiste ${eleccionUsuario}. La PC eligio ${eleccionPC}. ${resultado}!`);
-
-}while(confirm('¿Quieres jugar otra vez?'));
